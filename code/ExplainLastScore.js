9a7825f4c1ead07cda55c503126395f50d7d2804
Siamak Hodjat <shodjat@viv.ai>
2019-04-04 16:58:45 -0700

    added back image for the funny quiz

--- code/ExplainLastScore.js
+++
@@ -1,5 +0,0 @@
-//The last score is already explained when we get here (on the action's confirmation view), just mark the last score as explained.
-exports.function = function(state) {
-  state.lastScore.explained = true
-  return state
-}
