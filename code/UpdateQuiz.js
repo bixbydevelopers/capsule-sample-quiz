35e062271988ddc550318bfb3fde4a65a0f9d8e4
Roger Kibbe <rkibbe@viv.ai>
2019-10-01 19:11:52 -0700

    ASR fix for letters, reprompt

--- code/UpdateQuiz.js
+++ code/UpdateQuiz.js
@@ -1,35 +1,96 @@
-const { buildQuestionToSpeak } = require("./lib/util");
+const util = require("./lib/util");
 
-module.exports.function = function updateQuiz (quiz, answer) {
+// Will repromt if the user does not give a valid input. Set to false to not reprompt
+// Futue enhancement would be to reprompt only n times and then give up (suggest n = 1 or 2)
+const reprompt = true;
+
+// ASR Aliases to handle ASR variations for "A", "B" "C" or "D" etc
+const ASRaliases = {
+  "a": "a",
+  "hey": "a",
+  "b": "b",
+  "be": "b",
+  "bee": "b",
+  "c": "c",
+  "see": "c",
+  "sea": "c",
+  "si": "c",
+  "d": "d",
+  "t": "d",
+  "de": "d",
+}
+
+var console = require('console')
+
+module.exports.function = function updateQuiz(quiz, answer) {
+
+  answer = answer.toLowerCase();
   const i = quiz.index;
+  const options = quiz.questions[i].options;
+  const hasOptions = !(options === undefined || options.length == 0)
   const correctAnswers = quiz.questions[i].correctAnswer.acceptedAnswers;
-  const currentQuestion = quiz.questions[quiz.index];
-  
-  currentQuestion.options.forEach(o=>{
-    if(answer.toLowerCase() == o.alias.toLowerCase()){
-      answer = o.text;
-    }
-  });
-  
+  const correctAlias = String(quiz.questions[i].correctAnswer.acceptedAlias).toLowerCase();
+  const currentQuestion = quiz.questions[i];
   quiz.questions[i].answer = answer;
+  quiz.questions[quiz.index].invalidAnswer = false;
   var correct = false;
-  correctAnswers.forEach(o=>{
-    if(o.toLowerCase() == answer.toLowerCase()){
+  var answeredOption = false;
+
+  // Check for match with answer text e.g. "California"
+  // Matches answer only and answer in sentence 
+  correctAnswers.forEach(o => {
+    lowerO = o.toLowerCase()
+    corrAnsRegExp = lowerO + "| " + lowerO + " |^" + lowerO + " | " + lowerO + "$";
+    if (answer.match(corrAnsRegExp)) {
       correct = true;
     }
   });
-  
-  if(correct){
+
+  var possOptionAlias = []
+  if (!correct && hasOptions) { // Check if user answered an incorrect option text, also contruct possible aliases
+    options.forEach(o => {
+      lowerO = o.text.toLowerCase();
+      optionsRegExp = lowerO + "| " + lowerO + " |^" + lowerO + " | " + lowerO + "$";
+      if (answer.match(optionsRegExp)) {
+        answeredOption = true;
+      }
+      var x = o.alias.toLowerCase()
+      possOptionAlias.push(x)
+    })
+    
+    if (!answeredOption) { // Did user answer with an alias and was the alias correct
+      for (var key in ASRaliases) {
+        if (ASRaliases.hasOwnProperty(key)) {
+          if (possOptionAlias.indexOf(ASRaliases[key]) > -1) {
+            if (answer == key) {
+              // User answered with alias
+              answeredOption = true;
+              if (ASRaliases[key] == correctAlias) { // was alias correct answer
+                correct = true;
+              }
+            }
+          }
+        }
+      }
+    }
+
+  }
+
+  if (correct) {
     quiz.questions[quiz.index].correct = true;
     quiz.score++;
   }
-  
-  if(quiz.index < quiz.questions.length - 1){
-    quiz.textToSpeak = buildQuestionToSpeak(quiz.questions[i+1]);
+
+  if (!correct && !answeredOption && hasOptions && reprompt) {
+    // Answered with an invalid option. Reprompt
+    quiz.questions[quiz.index].invalidAnswer = true;
+    var validOptionsText = util.arrayToListForSpeech(possOptionAlias)
+    quiz.textToSpeak = util.buildQuestionToSpeak(quiz.questions[quiz.index]) + ". Please answer with a letter: " + validOptionsText || " ";
+  } else if (quiz.index < quiz.questions.length - 1) {
     quiz.index++;
+    quiz.textToSpeak = util.buildQuestionToSpeak(quiz.questions[quiz.index]) || " ";
   } else {
     quiz.completed = true;
   }
-  
   return quiz;
 }
--- code/data/quizzes.js
+++ code/data/quizzes.js
@@ -1,25 +1,25 @@
 module.exports = [
   // Start of funny quiz
-  {
-    title: "Funny Quiz",
-    tags: ["funny", "animal"], //used to find this quiz
-    image: {
-      url: "/images/Animals.jpeg",
-    },
-    questions: [
-      {
-        question: "What do cats like to eat on a hot day?",
-        options: ["Mice cream", "Ice Cream", "Hot Cream"],
-        answer: 0, // Corresponds to "Mice cream"
-      },
-      {
-        question: "What do you call a cold dog?",
-        options: ["Frozen", "Hot dog", "Chilli Dog"],
-        answer: "Chilli Dog",
-        explanation: "Because it's cold. Get it?",
-      },
-    ],
-  },
+  // {
+  //   title: "Funny Quiz",
+  //   tags: ["funny", "animal"], //used to find this quiz
+  //   image: {
+  //     url: "/images/Animals.jpeg",
+  //   },
+  //   questions: [
+  //     {
+  //       question: "What do cats like to eat on a hot day?",
+  //       options: ["Mice cream", "Ice Cream", "Hot Cream"],
+  //       answer: 0, // Corresponds to "Mice cream"
+  //     },
+  //     {
+  //       question: "What do you call a cold dog?",
+  //       options: ["Frozen", "Hot dog", "Chilli Dog"],
+  //       answer: "Chilli Dog",
+  //       explanation: "Because it's cold. Get it?",
+  //     },
+  //   ],
+  // },
   // End of funny quiz
 
   // Start of president quiz
@@ -58,75 +58,75 @@ module.exports = [
   // End of presidents quiz
 
   // Start of states quiz generated from the spreadsheet
-  {
-    title: "States Quiz",
-    tags: ["states", "state", "united states"],
-    image: { url: "/images/us_map.png" },
-    questions: [
-      {
-        question: "Which state has the nickname 'Spud State'?",
-        options: ["Idaho", "Wyoming", "Florida"],
-        answer: "Idaho",
-        explanation:
-          "Idaho is well known for growing potatoes and thus the nickname, Spud State",
-      },
-      {
-        question: "Which state has the nickname 'Peach State'?",
-        options: ["Hawaii", "California", "Georgia"],
-        answer: 2,
-      },
-      {
-        question: "Which state has the nickname 'Silver State'?",
-        options: ["New York", "Montana", "Nevada"],
-        answer: "Nevada",
-        explanation:
-          "Nevada had a silver rush in the mid 1800's and thus has the nickname, Silver State",
-      },
-      {
-        question: "What state is known as the Land of 10,000 lakes?",
-        options: ["Wisconsin", "Louisiana", "Minnesota"],
-        answer: "Minnesota",
-        explanation:
-          "Minnesota has over 11,000 lakes, so the land of 10,000 lakes is under counting!",
-      },
-      {
-        question: "What state is the largest in area?",
-        options: ["California", "Alaska", "Texas"],
-        answer: 1,
-        explanation:
-          "Alaska is the largest state by far with 665,384 square miles. Second place Texas has 268,596 square miles and third place California has 163,694 square miles",
-      },
-      {
-        question: "What state is the largest agricultural producer?",
-        options: ["Iowa", "Kansas", "California"],
-        answer: "California",
-        explanation:
-          "Caifornia produces over 11% of the nation's agricultural output followed by #2 Iowa which produces over 8%.",
-      },
-      {
-        question: "Which of the following states does not have a panhandle?",
-        options: ["Idaho", "Florida", "New Mexico"],
-        answer: "New Mexico",
-      },
-      {
-        question: "Which state does not border the Great Lakes?",
-        options: ["Ohio", "Michigan", "Iowa"],
-        answer: "Iowa",
-      },
-      {
-        question: "What is the smallest US state?",
-        options: ["Delaware", "Rhode Island", "Connecticut"],
-        answer: "Rhode Island",
-        explanation:
-          "Rhode Island is the smallest US state at 1,045 square miles. The next smallest is Delaware which at 1,954 square miles is almost twice the size of Rhode Island",
-      },
-      {
-        question: "How many states are prefixed with 'New'?",
-        answer: ["four", "4"],
-        explanation:
-          "There are four states prefixed by 'New': New Hampshire, New Jersey, New Mexico and New York",
-      },
-    ],
-  },
+  // {
+  //   title: "States Quiz",
+  //   tags: ["states", "state", "united states"],
+  //   image: { url: "/images/us_map.png" },
+  //   questions: [
+  //     {
+  //       question: "Which state has the nickname 'Spud State'?",
+  //       options: ["Idaho", "Wyoming", "Florida"],
+  //       answer: "Idaho",
+  //       explanation:
+  //         "Idaho is well known for growing potatoes and thus the nickname, Spud State",
+  //     },
+  //     {
+  //       question: "Which state has the nickname 'Peach State'?",
+  //       options: ["Hawaii", "California", "Georgia"],
+  //       answer: 2,
+  //     },
+  //     {
+  //       question: "Which state has the nickname 'Silver State'?",
+  //       options: ["New York", "Montana", "Nevada"],
+  //       answer: "Nevada",
+  //       explanation:
+  //         "Nevada had a silver rush in the mid 1800's and thus has the nickname, Silver State",
+  //     },
+  //     {
+  //       question: "What state is known as the Land of 10,000 lakes?",
+  //       options: ["Wisconsin", "Louisiana", "Minnesota"],
+  //       answer: "Minnesota",
+  //       explanation:
+  //         "Minnesota has over 11,000 lakes, so the land of 10,000 lakes is under counting!",
+  //     },
+  //     {
+  //       question: "What state is the largest in area?",
+  //       options: ["California", "Alaska", "Texas"],
+  //       answer: 1,
+  //       explanation:
+  //         "Alaska is the largest state by far with 665,384 square miles. Second place Texas has 268,596 square miles and third place California has 163,694 square miles",
+  //     },
+  //     {
+  //       question: "What state is the largest agricultural producer?",
+  //       options: ["Iowa", "Kansas", "California"],
+  //       answer: "California",
+  //       explanation:
+  //         "Caifornia produces over 11% of the nation's agricultural output followed by #2 Iowa which produces over 8%.",
+  //     },
+  //     {
+  //       question: "Which of the following states does not have a panhandle?",
+  //       options: ["Idaho", "Florida", "New Mexico"],
+  //       answer: "New Mexico",
+  //     },
+  //     {
+  //       question: "Which state does not border the Great Lakes?",
+  //       options: ["Ohio", "Michigan", "Iowa"],
+  //       answer: "Iowa",
+  //     },
+  //     {
+  //       question: "What is the smallest US state?",
+  //       options: ["Delaware", "Rhode Island", "Connecticut"],
+  //       answer: "Rhode Island",
+  //       explanation:
+  //         "Rhode Island is the smallest US state at 1,045 square miles. The next smallest is Delaware which at 1,954 square miles is almost twice the size of Rhode Island",
+  //     },
+  //     {
+  //       question: "How many states are prefixed with 'New'?",
+  //       answer: ["four", "4"],
+  //       explanation:
+  //         "There are four states prefixed by 'New': New Hampshire, New Jersey, New Mexico and New York",
+  //     },
+  //   ],
+  // },
   // End of States quiz
 ];
