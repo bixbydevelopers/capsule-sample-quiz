b04c36824e07e2349accb24abb8662917b7964f4
Roger Kibbe <32469292+rogerkibbe@users.noreply.github.com>
2019-10-06 14:42:04 -0700

    ASR fix and reprompt functionality (#17)
    * ASR fix for letters, reprompt
    
    * ASR fix for letters, reprompt
    
    * asr and reprompt
    
    * misc code cleanup
    
    * misc code cleanup
    
    * misc updates

--- code/UpdateQuiz.js
+++ code/UpdateQuiz.js
@@ -1,35 +1,57 @@
-const { buildQuestionToSpeak } = require("./lib/util");
+const util = require("./lib/util");
 
-module.exports.function = function updateQuiz (quiz, answer) {
+// Will repromt if the user does not give a valid input. Set to false to not reprompt
+// Futue enhancement would be to reprompt only n times and then give up (suggest n = 1 or 2)
+const reprompt = true;
+
+var console = require('console')
+
+module.exports.function = function updateQuiz(quiz, answer) {
+
+  answer = answer.toLowerCase();
   const i = quiz.index;
+  const options = quiz.questions[i].options;
+  const hasOptions = !(options === undefined || options.length == 0)
   const correctAnswers = quiz.questions[i].correctAnswer.acceptedAnswers;
-  const currentQuestion = quiz.questions[quiz.index];
-  
-  currentQuestion.options.forEach(o=>{
-    if(answer.toLowerCase() == o.alias.toLowerCase()){
-      answer = o.text;
-    }
-  });
-  
+  const correctAlias = String(quiz.questions[i].correctAnswer.acceptedAlias).toLowerCase();
+  const currentQuestion = quiz.questions[i];
   quiz.questions[i].answer = answer;
+  quiz.questions[quiz.index].invalidAnswer = false;
   var correct = false;
-  correctAnswers.forEach(o=>{
-    if(o.toLowerCase() == answer.toLowerCase()){
-      correct = true;
+  var answeredOption = false;
+
+  // Check for match with answer option e.g. "California" or "B Calfornia" assuming B was the option letter
+  // Matches answer only and answer in sentence 
+  correct = util.checkAnswerMatch(correctAnswers, answer)
+
+  var possOptionAlias = []
+  if (!correct && hasOptions) { // Check if user answered an incorrect option text, also contruct possible aliases
+    ret = util.checkIncorrectOption(options, answer)
+    answeredOption = ret.answeredOption;
+    possOptionAlias = ret.possOptionAlias
+
+    if (!answeredOption) { // Did user answer with an alias and was the alias correct
+      ret = util.checkAliasMatch(answer, possOptionAlias, correctAlias)
+      answeredOption = ret.answeredOption;
+      correct = ret.correct
     }
-  });
-  
-  if(correct){
+  }
+
+  if (correct) {
     quiz.questions[quiz.index].correct = true;
     quiz.score++;
   }
-  
-  if(quiz.index < quiz.questions.length - 1){
-    quiz.textToSpeak = buildQuestionToSpeak(quiz.questions[i+1]);
+
+  if (!correct && !answeredOption && hasOptions && reprompt) {
+    // Answered with an invalid option. Reprompt
+    quiz.questions[quiz.index].invalidAnswer = true;
+    var validOptionsText = util.arrayToListForSpeech(possOptionAlias)
+    quiz.textToSpeak = util.buildQuestionToSpeak(quiz.questions[quiz.index]) + ". Please answer with a letter: " + validOptionsText || " ";
+  } else if (quiz.index < quiz.questions.length - 1) {
     quiz.index++;
+    quiz.textToSpeak = util.buildQuestionToSpeak(quiz.questions[quiz.index]) || " ";
   } else {
     quiz.completed = true;
   }
-  
   return quiz;
 }
--- code/lib/util.js
+++ code/lib/util.js
@@ -2,7 +2,7 @@ var console = require("console");
 
 function findItems(items, searchTerm) {
   var matches = []
-  for (var i=0; i<items.length; i++) {
+  for (var i = 0; i < items.length; i++) {
     if (matchTag(items[i].tags, searchTerm)) {
       matches.push(items[i])
     }
@@ -12,7 +12,7 @@ function findItems(items, searchTerm) {
 
 //returns true if there is an element in a list of tags that matches a specific tag
 function matchTag(tags, tag) {
-  for (var i=0; i<tags.length; i++) {
+  for (var i = 0; i < tags.length; i++) {
     if (tag.toLowerCase() == tags[i].toLowerCase()) {
       return true
     }
@@ -22,16 +22,16 @@ function matchTag(tags, tag) {
 
 function buildQuestionsFromJson(quizJson) {
   var questions = []
-  for (var i=0; i<quizJson.questions.length; i++) {
+  for (var i = 0; i < quizJson.questions.length; i++) {
     questions.push(buildQuestionFromJson(quizJson.questions[i], i))
   }
   return questions
 }
 
-function buildCorrectAnswerText(question){
-  if(Array.isArray(question.answer)){
+function buildCorrectAnswerText(question) {
+  if (Array.isArray(question.answer)) {
     return question.answer[0];
-  } else if (typeof question.answer === 'number'){
+  } else if (typeof question.answer === 'number') {
     return question.options[question.answer];
   } else if (typeof question.answer === 'string') {
     return question.answer;
@@ -44,18 +44,22 @@ function buildQuestionFromJson(questionJson) {
   }
   var options = []
   if (questionJson.options) {
-    for (var i=0; i<questionJson.options.length; i++) {
+    for (var i = 0; i < questionJson.options.length; i++) {
       options.push({
         text: questionJson.options[i],
         alias: String.fromCharCode('A'.charCodeAt(0) + i)
       })
     }
   }
+
+  var acceptedAnswers = buildAcceptedAnswers(questionJson.answer, options);
+
   var question = {
     text: questionJson.question,
     options: options,
     correctAnswer: {
-      acceptedAnswers: buildAcceptedAnswers(questionJson.answer, options),
+      acceptedAnswers: acceptedAnswers.answers,
+      acceptedAlias: acceptedAnswers.alias,
       explanation: questionJson.explanation,
       text: buildCorrectAnswerText(questionJson)
     }
@@ -67,46 +71,72 @@ function buildQuestionFromJson(questionJson) {
 function buildQuestionToSpeak(question) {
   var options = question.options
   optionsString = ''
-  for (var i=0; i< options.length; i++) {
-    optionsString += options[i].alias + '... ' + options[i].text + (i+1 < options.length ? ', ... ' : '')
+  for (var i = 0; i < options.length; i++) {
+    optionsString += options[i].alias + '... ' + options[i].text + (i + 1 < options.length ? ', ... ' : '')
   }
   return optionsString;
 }
 
 function buildAcceptedAnswers(answer, options) {
   var acceptedAnswers = []
+  var alias;
+
   if (Array.isArray(answer)) { //is answer an array?
-    for (var i=0; i<answer.length; i++) {
-      acceptedAnswers = acceptedAnswers.concat(buildAcceptedAnswers(answer[i], options))
+    for (var i = 0; i < answer.length; i++) {
+      var acceptedAnswer = buildAcceptedAnswer(answer[i], options)
+      acceptedAnswers = acceptedAnswers.concat(acceptedAnswer.answer);
+      if (acceptedAnswer.alias) {
+        alias = acceptedAnswer.alias;
+      }
     }
-  } else if (typeof answer === 'number' && options && answer < options.length) {
-    acceptedAnswers.push(options[answer].alias)
-    acceptedAnswers.push(options[answer].text)
+  } else {
+    var acceptedAnswer = buildAcceptedAnswer(answer, options)
+    acceptedAnswers = acceptedAnswer.answer;
+    if (acceptedAnswer.alias) {
+      alias = acceptedAnswer.alias;
+    }
+  }
+  return {
+    answers: acceptedAnswers,
+    alias: alias
+  }
+}
+
+function buildAcceptedAnswer(answer, options) {
+  var acceptedAnswer
+  var alias
+  if (typeof answer === 'number' && options && answer < options.length) {
+    alias = options[answer].alias
+    acceptedAnswer = options[answer].text
   } else if (answer) {
-    acceptedAnswers.push(answer)
-    //also push all aliases matching the answer
+    acceptedAnswer = answer
+    //also find alias matching the answer
     if (options) {
-      for (var i=0; i<options.length; i++) {
+      for (var i = 0; i < options.length; i++) {
         if (options[i].text == answer) {
-          acceptedAnswers.push(options[i].alias)
+          alias = options[i].alias;
         }
       }
     }
   }
-  return acceptedAnswers
+
+  return {
+    answer: acceptedAnswer,
+    alias: alias
+  }
 }
 
-function buildQuizzes(searchTerm){
+function buildQuizzes(searchTerm) {
   var quizzes = require("../data/quizzes");
   if (searchTerm) {
     const foundQuiz = findItems(quizzes, searchTerm)
-    if(foundQuiz.length > 0){
+    if (foundQuiz.length > 0) {
       quizzes = foundQuiz;
     }
   }
   var formattedQuizzes = [];
   //read the questions in the quiz and initialize the state
-  for (var i=0; i<quizzes.length; i++) {
+  for (var i = 0; i < quizzes.length; i++) {
     var quiz = quizzes[i];
     quiz.completed = false;
     quiz.score = 0;
@@ -124,7 +154,87 @@ function buildQuizzes(searchTerm){
   return formattedQuizzes;
 }
 
+function arrayToListForSpeech(input) {
+  var output = input.join();
+  output = output.replace(/,/g, '...');
+  output = output.substring(0, output.lastIndexOf("...")) + "... or " + output.substring(output.lastIndexOf("...") + 3, output.length) + "..."
+  output = output.toUpperCase();
+  return output;
+}
+
+// ASR Aliases to handle ASR variations for "A", "B" "C" or "D" etc
+// This is a temporary workaround for an in progress bug that will be fixed
+const inputAliases = {
+  "a": "a",
+  "hey": "a",
+  "b": "b",
+  "be": "b",
+  "bee": "b",
+  "c": "c",
+  "see": "c",
+  "sea": "c",
+  "si": "c",
+  "d": "d",
+  "t": "d",
+  "de": "d",
+}
+// Regular expresion to find ASR alias - creatd by ASRaliases const
+const aliasesRE = "^(" + String(Object.getOwnPropertyNames(inputAliases)).replace('/"/g', '').replace(/,/g, "|") + ")";
+
+function checkAnswerMatch(correctAnswers, answer) {
+  var correct = false;
+  correctAnswers.forEach(o => {
+    var lowerO = o.toLowerCase()
+    var corrAnsRegExp = "^" + lowerO + "$|" + aliasesRE + " " + lowerO + "$"
+    if (answer.match(corrAnsRegExp)) {
+      correct = true;
+    }
+  });
+  return correct;
+}
+
+function checkIncorrectOption(options, answer) {
+  var answeredOption = false;
+  var possOptionAlias = []
+  options.forEach(o => {
+    lowerO = o.text.toLowerCase();
+    optionsRegExp = lowerO + "| " + lowerO + " |^" + lowerO + " | " + lowerO + "$";
+    if (answer.match(optionsRegExp)) {
+      answeredOption = true;
+    }
+    var x = o.alias.toLowerCase()
+    possOptionAlias.push(x)
+  })
+
+  return {
+    answeredOption: answeredOption,
+    possOptionAlias: possOptionAlias
+  }
+}
+
+function checkAliasMatch(answer, possOptionAlias, correctAlias) {
+  var answeredOption = false;
+  var correct = false;
+  var aliasMatch = inputAliases[answer];
+  if (aliasMatch) {
+    if (possOptionAlias.indexOf(aliasMatch) > -1) {
+      answeredOption = true;
+      if (aliasMatch == correctAlias) {
+        correct = true;
+      }
+    }
+  }
+  return {
+    answeredOption: answeredOption,
+    correct: correct
+  }
+}
+
 module.exports = {
-  buildQuizzes:buildQuizzes,
-  buildQuestionToSpeak:buildQuestionToSpeak
+  buildQuizzes: buildQuizzes,
+  buildQuestionToSpeak: buildQuestionToSpeak,
+  arrayToListForSpeech: arrayToListForSpeech,
+  checkAnswerMatch: checkAnswerMatch,
+  checkIncorrectOption: checkIncorrectOption,
+  checkAliasMatch: checkAliasMatch,
 }
